49402dd479f3887ccc6f9588465445124aa1f750
JustinBacher <92jbach@gmail.com>
2024-11-04 12:33:29 -0500

    fix motion

--- dot_config/nvim/lua/plugins/motion.lua
+++ dot_config/nvim/lua/plugins/motion.lua
@@ -1,11 +1,3 @@
-local harpoon_keys = {}
-for i = 1, 10 do
-	table.insert(
-		harpoon_keys,
-		{ "<C-" .. i .. ">", function() require("harpoon"):list():select(i) end, desc = "Harpoon File " .. i }
-	)
-end
-
 return {
 	{
 		"folke/flash.nvim",
@@ -51,13 +43,6 @@ return {
 		},
 	},
 	{
-<<<<<<< HEAD
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		keys = vim.tbl_extend("force", harpoon_keys, {
-			{ "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon: Mark File" },
-=======
 		"mrjones2014/smart-splits.nvim",
 		opts = { ignored_filetypes = { "NvimTree" } },
 		setup = function()
@@ -77,19 +62,26 @@ return {
 	{
 		"letieu/harpoon-lualine",
 		dependencies = {
->>>>>>> a745be2 (changing how i do multiplexing)
 			{
-				"<C-e>",
-				function()
-					local h = require("harpoon")
-					h.ui:toggle_quick_menu(h:list())
-				end,
-				desc = "Toggle Harpoon Menu",
+				"ThePrimeagen/harpoon",
+				branch = "harpoon2",
+				dependencies = { "nvim-lua/plenary.nvim" },
+				keys = {
+					{ "<leader>a", function() require("harpoon"):list():add() end, desc = "Harpoon: Mark File" },
+					{
+						"<C-e>",
+						function()
+							local h = require("harpoon")
+							h.ui:toggle_quick_menu(h:list())
+						end,
+						desc = "Toggle Harpoon Menu",
+					},
+					{ "<C-t>", function() require("harpoon"):list():select(1) end, desc = "Harpoon File 1" },
+					{ "<C-s>", function() require("harpoon"):list():select(2) end, desc = "Harpoon File 2" },
+					{ "<C-b>", function() require("harpoon"):list():select(3) end, desc = "Harpoon File 3" },
+					{ "<C-g>", function() require("harpoon"):list():select(4) end, desc = "Harpoon File 4" },
+				},
 			},
-		}),
-	},
-	{
-		"letieu/harpoon-lualine",
-		dependencies = { "ThePrimeagen/harpoon" },
+		},
 	},
 }
