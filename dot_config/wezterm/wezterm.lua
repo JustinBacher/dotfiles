1ef481bf5d1ef61cb70c66f3d94ac6e399f7786c
Justin Bacher <92jbach@gmail.com>
2024-11-04 13:15:38 -0500

    revert to the non-confusing lua instead of tmpl file

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = {} or wezterm.config_builder()
 local act = wezterm.action
+local is_linux = wezterm.target_triple:find("linux") ~= nil
+
+local is_darwin = wezterm.target_triple:find("darwin")
 
 -- ██████╗ ██╗     ██╗   ██╗ ██████╗ ██╗███╗   ██╗███████╗
 -- ██╔══██╗██║     ██║   ██║██╔════╝ ██║████╗  ██║██╔════╝
@@ -17,7 +20,6 @@ smart_splits.apply_to_config(config, {
 		resize = "CTRL|SHIFT",
 	},
 })
-
 
 --  ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗
 -- ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝
@@ -47,10 +49,10 @@ config.visual_bell = {
 }
 
 -- Wayland fix
-{{ if eq .displayServer "wayland" }}
-config.enable_wayland = true
-config.front_end = "OpenGL"
-{{- end }}
+if os.getenv("XDG_SESSION_TYPE") == "wayland" then
+	config.enable_wayland = true
+	config.front_end = "OpenGL"
+end
 
 -- ███████╗ ██████╗ ███╗   ██╗████████╗
 -- ██╔════╝██╔═══██╗████╗  ██║╚══██╔══╝
@@ -60,16 +62,16 @@ config.front_end = "OpenGL"
 -- ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ╚═╝
 
 config.font = wezterm.font_with_fallback({
-	{ family = "ComicShannsMono Nerd Font Mono", weight = "Regular", stretch="Normal", style="Normal" },
-	{ family = "VictorMono Nerd Font", weight = "Regular", stretch="Normal", style="Italic" },
+	{ family = "ComicShannsMono Nerd Font Mono", weight = "Regular", stretch = "Normal", style = "Normal" },
+	{ family = "VictorMono Nerd Font", weight = "Regular", stretch = "Normal", style = "Italic" },
 	{ family = "Symbols Nerd Font", weight = "Regular" },
 })
 
-{{- if eq .chezmoi.os "darwin" }}
-config.font_size = 15
-{{- else }}
-config.font_size = 13
-{{- end }}
+if is_darwin then
+	config.font_size = 15
+else
+	config.font_size = 13
+end
 
 --  ██████╗██╗   ██╗██████╗ ███████╗ ██████╗ ██████╗
 -- ██╔════╝██║   ██║██╔══██╗██╔════╝██╔═══██╗██╔══██╗
@@ -92,48 +94,64 @@ config.cursor_blink_ease_out = "EaseOut"
 
 local move_around = function(window, pane, direction_wez, direction_nvim)
 	if pane:get_title():lower() == "nvim" then
-		window:perform_action(wezterm.action{SendString="\x17"..direction_nvim}, pane)
+		window:perform_action(wezterm.action({ SendString = "\x17" .. direction_nvim }), pane)
 	else
-		window:perform_action(wezterm.action{ActivatePaneDirection=direction_wez}, pane)
+		window:perform_action(wezterm.action({ ActivatePaneDirection = direction_wez }), pane)
 	end
 end
 
 config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
 	-- Splitting panes
-	{ key = "h",      mods = "ALT",  action = act.Multiple { act.SplitPane { direction = "Left", size = { Percent = 50 } } } },
-	{ key = "j",      mods = "ALT",  action = act.Multiple { act.SplitPane { direction = "Down", size = { Percent = 50 } } } },
-	{ key = "k",      mods = "ALT",  action = act.Multiple { act.SplitPane { direction = "Down", size = { Percent = 50 } } } },
-	{ key = "l",      mods = "ALT",  action = act.Multiple { act.SplitPane { direction = "Right", size = { Percent = 50 } } } },
+	{
+		key = "h",
+		mods = "ALT",
+		action = act.Multiple({ act.SplitPane({ direction = "Left", size = { Percent = 50 } }) }),
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = act.Multiple({ act.SplitPane({ direction = "Down", size = { Percent = 50 } }) }),
+	},
+	{
+		key = "k",
+		mods = "ALT",
+		action = act.Multiple({ act.SplitPane({ direction = "Down", size = { Percent = 50 } }) }),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act.Multiple({ act.SplitPane({ direction = "Right", size = { Percent = 50 } }) }),
+	},
 
 	-- Focusing Panes
-	{ key = "h",      mods = "CTRL", action = act.ActivatePaneDirection "Left" },
-	{ key = "j",      mods = "CTRL", action = act.ActivatePaneDirection "Down" },
-	{ key = "k",      mods = "CTRL", action = act.ActivatePaneDirection "Up" },
-	{ key = "l",      mods = "CTRL", action = act.ActivatePaneDirection "Right" },
+	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
 
-	{ key = "Escape", mods = "ALT",  action = act.CloseCurrentPane { confirm = false } },
-	{ key = "r", mods = "LEADER",  action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+	{ key = "Escape", mods = "ALT", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
 }
 
 -- leader + number to activate that tab
 for i = 0, 9 do
-	table.insert(config.keys, { key = tostring(i), mods = "ALT", action = act.ActivateTab(i), })
+	table.insert(config.keys, { key = tostring(i), mods = "ALT", action = act.ActivateTab(i) })
 end
 
 config.key_tables = {
 	resize_pane = {
-		{ key = "LeftArrow",  action = act.AdjustPaneSize { "Left", 3 } },
-		{ key = "h",          action = act.AdjustPaneSize { "Left", 3 } },
-		{ key = "RightArrow", action = act.AdjustPaneSize { "Right", 3 } },
-		{ key = "l",          action = act.AdjustPaneSize { "Right", 3 } },
-		{ key = "UpArrow",    action = act.AdjustPaneSize { "Up", 3 } },
-		{ key = "k",          action = act.AdjustPaneSize { "Up", 3 } },
-		{ key = "DownArrow",  action = act.AdjustPaneSize { "Down", 3 } },
-		{ key = "j",          action = act.AdjustPaneSize { "Down", 3 } },
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 3 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 3 }) },
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 3 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 3 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 3 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 3 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 3 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 3 }) },
 
 		-- Cancel the mode by pressing escape
-		{ key = "Escape",     action = "PopKeyTable" },
+		{ key = "Escape", action = "PopKeyTable" },
 	},
 }
 
@@ -143,20 +161,20 @@ wezterm.on("update-right-status", function(window, _)
 	local prefix = ""
 
 	if window:leader_is_active() then
-		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+		prefix = " " .. utf8.char(0x1f30a) --- @diagnostic disable-line undefined-global
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2) --- @diagnostic disable-line undefined-global
 	end
 
 	if window:active_tab():tab_id() ~= 0 then
 		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
 	end -- arrow color based on if tab is first pane
 
-	window:set_left_status(wezterm.format {
+	window:set_left_status(wezterm.format({
 		{ Background = { Color = "#b7bdf8" } },
 		{ Text = prefix },
 		ARROW_FOREGROUND,
-		{ Text = SOLID_LEFT_ARROW }
-	})
+		{ Text = SOLID_LEFT_ARROW },
+	}))
 end)
 
 -- ██╗    ██╗██╗███╗   ██╗██████╗  ██████╗ ██╗    ██╗
@@ -164,14 +182,14 @@ end)
 -- ██║ █╗ ██║██║██╔██╗ ██║██║  ██║██║   ██║██║ █╗ ██║
 -- ██║███╗██║██║██║╚██╗██║██║  ██║██║   ██║██║███╗██║
 -- ╚███╔███╔╝██║██║ ╚████║██████╔╝╚██████╔╝╚███╔███╔╝
- -- ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝  ╚══╝╚══╝ 
+-- ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝  ╚══╝╚══╝
 
 -- This is because we are using a tiling window manager
-{{- if eq .chezmoi.os "darwin" }}
-config.window_decorations = "RESIZE"
-{{- else }}
-config.window_decorations = "NONE"
-{{- end }}
+if is_darwin then
+	config.window_decorations = "RESIZE"
+else
+	config.window_decorations = "NONE"
+end
 
 -- tab bar
 config.hide_tab_bar_if_only_one_tab = true
@@ -186,18 +204,17 @@ config.use_fancy_tab_bar = true
 
 config.color_scheme = "Tokyo Night Moon"
 
-{{- if eq .chezmoi.os "darwin" }}
-config.window_background_opacity = 0.7
-config.macos_window_background_blur = 20
-{{- else if eq .chezmoi.os "linux" }}
-config.window_background_opacity = 0.0
-{{- else }}
-config.window_background_opacity = 0.6
-{{- end }}
+if is_darwin then
+	config.window_background_opacity = 0.7
+	config.macos_window_background_blur = 20
+elseif is_linux then
+	config.window_background_opacity = 0.0
+	config.window_background_opacity = 0.6
+end
 
 config.inactive_pane_hsb = {
-  saturation = 0.8,
-  brightness = 0.3,
+	saturation = 0.8,
+	brightness = 0.3,
 }
 
 -- Ensure the wezterm TERM definitions are installed for wezterm TERM to work
